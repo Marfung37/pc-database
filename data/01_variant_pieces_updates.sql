81175110fbc1ad8e5fc62bf0de59cb804501f324
Marvin Fung <marvin.fung37@gmail.com>
2025-05-20 01:10:25 -0500

    Adds trigger to check if variant pieces is set if not oqb setup

---
+++ data/01_variant_pieces_updates.sql
@@ -0,0 +1,19 @@
+-- Making sure the pieces is set if not oqb
+CREATE OR REPLACE FUNCTION check_variant_pieces()
+RETURNS TRIGGER
+AS $$
+BEGIN
+  IF NEW.pieces IS NULL AND EXISTS (
+      SELECT 1 FROM setups s
+      WHERE s.setup_id = NEW.setup_id AND s.oqb_path IS NULL
+  ) THEN
+      RAISE EXCEPTION 'Variant must set pieces if not oqb setup';
+  END IF;
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_check_variant_pieces
+BEFORE INSERT OR UPDATE ON setup_variants
+FOR EACH ROW
+EXECUTE FUNCTION check_variant_pieces();
