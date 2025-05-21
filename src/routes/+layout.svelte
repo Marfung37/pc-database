2ec7bee1b96a215a46a569b0ea8f854bfe51929f
Marvin Fung <marvin.fung37@gmail.com>
2025-05-21 15:27:16 -0500

    Basic login setup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,24 @@
 <script lang="ts">
   import '../app.css';
+	import { invalidate } from '$app/navigation'
+	import { onMount } from 'svelte'
 
-  let { children } = $props();
+  export let data;
+
+	let { supabase, session } = data;
+	$: ({ supabase, session } = data);
+
+	onMount(() => {
+		const { data } = supabase.auth.onAuthStateChange((event, newSession) => {
+			if (newSession?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth')
+			}
+		})
+
+		return () => data.subscription.unsubscribe()
+	})
 </script>
 
-{@render children()}
+<div class="container" style="padding: 50px 0 100px 0">
+  <slot />
+</div>
---
+++ src/routes/+page.server.ts
@@ -0,0 +1,40 @@
+import { fail, redirect } from '@sveltejs/kit'
+import type { Actions, PageServerLoad } from './$types'
+
+export const load: PageServerLoad = async ({ url, locals: { safeGetSession } }) => {
+  const { session } = await safeGetSession()
+
+  // if the user is already logged in return them to the account page
+  if (session) {
+    redirect(303, '/account')
+  }
+
+  return { url: url.origin }
+}
+
+export const actions: Actions = {
+  default: async ({request, locals: { supabase } }) => {
+    const formData = await request.formData()
+    const email = formData.get('email') as string
+    const password = formData.get('password') as string
+
+    const validEmail = /^[\w-\.+]+@([\w-]+\.)+[\w-]{2,8}$/.test(email)
+    if (!validEmail) {
+      return fail(400, { errors: { email: 'Please enter a valid email address' }, email })
+    }
+
+    const { error } = await supabase.auth.signInWithPassword({ email, password })
+
+    if (error) {
+      return fail(400, {
+        success: false,
+        email,
+        message: `There was an issue, Please contact support.`,
+      })
+    }
+
+    return {
+      success: true,
+    }
+  },
+}
