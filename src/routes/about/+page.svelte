19d2f6967950e73ad0b64ed9098d893d4e4d0e26
Marvin Fung <marvin.fung37@gmail.com>
2025-05-22 12:57:21 -0500

    Clean up navbar a bit more

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { m } from '$lib/paraglide/messages.js';
+</script>
+
+<div class="hero min-h-[60vh]">
+  <div class="hero-content text-center py-12">
+    <div class="max-w-xl">
+      <div
+        class="text-xl md:text-3xl font-bold bg-clip-text text-transparent bg-linear-to-r from-primary to-accent mb-3 md:mb-7 pb-1"
+      >
+        WIP: {m.nav_about()}
+      </div>
+    </div>
+  </div>
+</div>
