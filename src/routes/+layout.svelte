5747327c51d6d8cb176bc4872e1e04542aaceb49
Marvin Fung <marvin.fung37@gmail.com>
2025-05-22 01:15:50 -0500

    Merges some of the SaaS template

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import '../app.css';
+  import { WebsiteName } from "$lib/config"
   import { invalidate } from '$app/navigation';
   import { onMount } from 'svelte';
 
@@ -19,6 +20,45 @@
   });
 </script>
 
-<div class="container" style="padding: 50px 0 100px 0">
+<div class="navbar bg-base-100 container mx-auto">
+  <div class="flex-1">
+    <a class="btn btn-ghost normal-case text-xl" href="/">{WebsiteName}</a>
+  </div>
+  <div class="flex-none">
+    <ul class="menu menu-horizontal px-1 hidden sm:flex font-bold text-lg">
+      <li class="md:mx-2"><a href="/database">Database</a></li>
+      <li class="md:mx-2"><a href="/account">Account</a></li>
+    </ul>
+    <div class="dropdown dropdown-end sm:hidden">
+      <!-- svelte-ignore a11y_label_has_associated_control -->
+      <!-- svelte-ignore a11y_no_noninteractive_tabindex -->
+      <label tabindex="0" class="btn btn-ghost btn-circle">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          ><path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M4 6h16M4 12h16M4 18h7"
+          /></svg
+        >
+      </label>
+      <!-- svelte-ignore a11y_no_noninteractive_tabindex -->
+      <ul
+        tabindex="0"
+        class="menu menu-lg dropdown-content mt-3 z-1 p-2 shadow-sm bg-base-100 rounded-box w-52 font-bold"
+      >
+        <li><a href="/database">Database</a></li>
+        <li><a href="/account">Account</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+<div class="">
   <slot />
 </div>
