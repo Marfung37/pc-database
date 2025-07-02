ff49c567d47f61d85490d612285fa42d87d990c9
Marvin Fung <marvin.fung37@gmail.com>
2025-07-02 11:35:55 -0500

    Adds sql function to find tasks for calculating saves data

---
+++ data/10_find_uncalculated_saves.sql
@@ -0,0 +1,55 @@
+CREATE OR REPLACE FUNCTION public.find_uncalculated_saves ()
+RETURNS TABLE (
+  setup_id setupid,
+  pc smallint,
+  leftover queue,
+  build queue,
+  hold smallint,
+  stat_id uuid,
+  kicktable kicktable,
+  hold_type hold_type,
+  save_id uuid,
+  save varchar(255),
+  description varchar(255),
+  auto_populate bool,
+  gen_minimal bool,
+  gen_all_solves bool
+)
+SET
+  search_path = public
+SECURITY INVOKER AS $$
+BEGIN
+  RETURN QUERY
+  SELECT
+    s.setup_id,
+    s.pc,
+    s.leftover,
+    s.build,
+    s.hold,
+    st.stat_id,
+    st.kicktable,
+    st.hold_type,
+    sa.save_id,
+    sa.save,
+    sa.description,
+    sa.auto_populate,
+    sa.gen_minimal,
+    sa.gen_all_solves
+  FROM
+    setups s
+  JOIN saves sa ON
+    s.pc = sa.pc
+  JOIN statistics st ON 
+    s.setup_id = st.setup_id
+  WHERE
+    sa.auto_populate = true
+    AND NOT EXISTS ( -- anti join for save data entry doesn't exist
+      SELECT 1
+      FROM save_data sd
+      WHERE sd.stat_id = st.stat_id
+        AND sd.save_id = sa.save_id
+    )
+  ORDER BY
+    s.setup_id, s.pc;
+END;
+$$ LANGUAGE plpgsql;
