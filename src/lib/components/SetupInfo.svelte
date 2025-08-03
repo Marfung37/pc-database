8d0da7039a680bfa9f75efa7fc5a75b3359c7e55
Marvin Fung <marvin.fung37@gmail.com>
2025-08-03 00:47:42 -0500

    Basic handling for priority saves

--- src/lib/components/SetupInfo.svelte
+++ src/lib/components/SetupInfo.svelte
@@ -42,10 +42,22 @@
         class="flex h-60 w-full flex-col flex-wrap gap-x-2 p-4 lg:w-auto lg:p-0 lg:py-12 lg:pl-8"
       >
         {#each setup.saves as save (save.save)}
+          {#if save.save_fraction}
           <p>
-            {save.description ?? save.save}: {save.save_percent.toFixed(2)}% ({save.save_fraction
-              .numerator}/{save.save_fraction.denominator})
+            {save.description ?? save.save}: {save.save_percent.toFixed(2)}% ({save.save_fraction.numerator}/{save.save_fraction.denominator})
           </p>
+          {:else}
+            <div>
+              <p>Priority Saves</p>
+              {#each save.priority_save_percent as save_percent, index}
+                {@const save_fraction = save.priority_save_fraction[index]}
+                {@const description = save.description?.split(', ')[index] ?? save.save.split(',')[index]}
+                <p class="pl-2">
+                  {description}: {save_percent.toFixed(2)}% ({save_fraction.numerator}/{save_fraction.denominator})
+                </p>
+              {/each}
+            </div>
+          {/if}
         {/each}
       </div>
     {/if}
