638cfc2dcc709f6264e1229cab71f76395d6b8cd
Marvin Fung <marvin.fung37@gmail.com>
2025-12-18 00:52:15 -0600

    Removes some latency in requesting for a setup

--- data/10_find_setup.sql
+++ data/10_find_setup.sql
@@ -17,7 +17,84 @@ CREATE TYPE setup_saves_data AS (
   true_miniaml boolean
 );
 
--- find a setup for solving using leftover
+-- bare minimal data to determine if a given setup is actually buildable based on leftover
+CREATE OR REPLACE FUNCTION public.find_bare_setup_leftover (
+  p_leftover queue,
+  kicktable kicktable DEFAULT 'srs180',
+  hold_type hold_type DEFAULT 'any'
+) RETURNS TABLE (
+  setup_id setupid,
+  build queue,
+  cover_pattern text,
+  cover_data bytea
+)
+SET
+  search_path = public,
+  extensions SECURITY INVOKER AS $$
+BEGIN
+  RETURN QUERY
+  SELECT 
+    s.setup_id,
+    s.build,
+    s.cover_pattern,
+    st.cover_data
+  FROM
+    setups s
+  JOIN statistics st ON 
+    s.setup_id = st.setup_id
+    AND st.kicktable = find_bare_setup_leftover.kicktable
+    AND st.hold_type = find_bare_setup_leftover.hold_type
+  LEFT JOIN setup_oqb_paths sop ON
+    s.setup_id = sop.setup_id
+  WHERE
+    s.leftover = find_bare_setup_leftover.p_leftover AND
+    (
+      s.type <> 'oqb' OR
+      (
+        s.type = 'oqb' AND sop.oqb_path = s.setup_id::ltree -- if oqb then only the root node
+      )
+    )
+  ORDER BY
+    s.setup_id;
+END;
+$$ LANGUAGE plpgsql;
+
+-- bare minimal data to determine if a given setup is actually buildable based on parent id
+CREATE OR REPLACE FUNCTION public.find_bare_setup_parent_id (
+  parent_id setupid,
+  kicktable kicktable DEFAULT 'srs180',
+  hold_type hold_type DEFAULT 'any'
+) RETURNS TABLE (
+  setup_id setupid,
+  build queue,
+  cover_pattern text,
+  cover_data bytea
+)
+SET
+  search_path = public,
+  extensions SECURITY INVOKER AS $$
+BEGIN
+  RETURN QUERY
+  SELECT 
+    s.setup_id,
+    s.build,
+    s.cover_pattern,
+    st.cover_data
+  FROM
+    setups s
+  JOIN statistics st ON 
+    s.setup_id = st.setup_id
+    AND st.kicktable = find_bare_setup_parent_id.kicktable
+    AND st.hold_type = find_bare_setup_parent_id.hold_type
+  JOIN setup_oqb_paths sop ON
+    s.setup_id = sop.setup_id AND
+    sop.oqb_path ~ (find_bare_setup_parent_id.parent_id::text || '.*{1}')::lquery
+  ORDER BY
+    s.setup_id;
+END;
+$$ LANGUAGE plpgsql;
+
+-- find a possibly invalid setup for solving using leftover
 CREATE OR REPLACE FUNCTION public.find_setup_leftover (
   p_leftover queue,
   p_include_variants boolean DEFAULT false,
@@ -225,15 +302,6 @@ BEGIN
     WHERE sd.stat_id = st.stat_id
       AND sd.status = 'completed'
   ) sa ON p_include_saves
-  WHERE
-    s.leftover = find_setup_leftover.p_leftover AND
-    (
-      s.type <> 'oqb' OR
-      (
-        s.type = 'oqb' AND sop.oqb_path = s.setup_id::ltree -- if oqb then only the root node
-      )
-    )
-
   ORDER BY
     s.setup_id;
 END;
@@ -241,7 +309,7 @@ $$ LANGUAGE plpgsql;
 
 -- find a setup for solving using setup_id
 CREATE OR REPLACE FUNCTION public.find_setup_setup_id (
-  p_setup_id setupid,
+  p_setup_ids setupid[],
   p_include_variants boolean DEFAULT false,
   p_include_saves boolean DEFAULT false,
   kicktable kicktable DEFAULT 'srs180',
@@ -299,11 +367,9 @@ BEGIN
     s.setup_id = st.setup_id
     AND st.kicktable = find_setup_setup_id.kicktable
     AND st.hold_type = find_setup_setup_id.hold_type
-  LEFT JOIN setup_variants v ON
-    s.setup_id = v.setup_id
   LEFT JOIN setup_translations sl ON
     s.setup_id = sl.setup_id AND sl.language_code = language
-  LEFT JOIN setup_translations sl ON
+  LEFT JOIN setup_translations sl_default ON
     s.setup_id = sl_default.setup_id AND sl_default.language_code = 'en'
   LEFT JOIN LATERAL (
     SELECT ARRAY_AGG(ROW(
@@ -337,7 +403,7 @@ BEGIN
       AND sd.status = 'completed'
   ) sa ON p_include_saves
   WHERE
-    s.setup_id = find_setup_setup_id.p_setup_id
+    s.setup_id = ANY(find_setup_setup_id.p_setup_ids)
   ORDER BY
     s.setup_id;
 END;
